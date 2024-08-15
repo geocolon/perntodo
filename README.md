620e78dc67473ef31c0b33e5b486b6ba6be70982
George Colon <geroge_colon@oneilluminate.com>
2024-08-15 10:52:15 -0700

    updated db.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Perntodo
 use nvm use 16 as the node version.
-server - dev = nodemon index.js --- DB psql -U postgres
+server - dev = nodemon index.js --- DB command - psql -U postgres
 client - npm start
--- server/db.js
+++ server/db.js
@@ -1,20 +1,20 @@
-// require("dotenv").config();
-
-// const pool = new Pool({
-//   host: "aws-0-us-east-1.pooler.supabase.com",
-//   port: 6543,
-//   user: "postgres",
-//   password: process.env.DB_PASSWORD,
-//   database: "postgres",
-// });
-const Pool = require('pg').Pool
+require("dotenv").config();
 
 const pool = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+  host: "aws-0-us-east-1.pooler.supabase.com",
+  port: 6543,
+  user: "postgres",
+  password: process.env.DB_PASSWORD,
+  database: "postgres",
 });
+// const Pool = require('pg').Pool
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "password",
+//     host: "localhost",
+//     port: 5432,
+//     database: "perntodo"
+// });
 
 module.exports = pool;
